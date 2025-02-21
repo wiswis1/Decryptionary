624af51c3b7b5879188e410578d75cbc163b2c5c
wiswis1 <elijahwidom6@gmail.com>
2025-02-21 16:47:20 -0600

    Create game.py file using dictionary modules to create our database of words and access their definitions Define functions to get user input and access database to run the game implementing cyphering/deciphering for game mechanic

--- game.py
+++ game.py
@@ -1,44 +1,93 @@
 # Import PyMultiDictionary
 from PyMultiDictionary import MultiDictionary
+import random
+import threading
 import time
 
-# set dictionary language and such
-dictionary = MultiDictionary()
-dictionary.set_words_lang('en')
-definition = dictionary.meaning('en',"hello")
-line = definition[1]
+# main game class !
+class game:
 
-lineSplit = line.split()
-print(''.join(lineSplit))
+    global dictionary
+    dictionary = MultiDictionary()
+    dictionary.set_words_lang('en')
 
-
-synonyms = dictionary.synonym('en', 'hello') 
-print(synonyms)
-
-
-for i in range(len(lineSplit)): 
-    if lineSplit[i].lower() == "hello" :
-        lineSplit[i] = "BLANK"
-    # check synonyms
-    for j in range(len(synonyms)):
-        if lineSplit[i] == synonyms[j]:
-            lineSplit[i] = "BLANK"
+   
+# get the synonyms of a word
+    def getSynonyms(self, word): 
+        synonyms = dictionary.synonym('en', word) 
+        return synonyms
         
-print(''.join(lineSplit))
+        
+    def getCipherDefinition(self, definition):
+        definition = definition.replace(".|\\,", " ")
+        print(definition)
+        listDef = definition.split()
+        shift = random.randint(1, 13)
+        alpha = "abcdefghijklmnopqrstuvwxyz"
+
+        for i in range(len(listDef)) :
+            if listDef[i].lower() == self.word.lower() :
+                cipheredWord = ""
+                # for all letters in listDef[i]
+                for k in range(len(listDef[i])):
+                   # for all letters in the alphabet
+                   for l in range(len(alpha)):
+                       # if the current letter matches this letter of the alphabet   
+                        if (alpha[l] == listDef[i][k]):
+                           # shift the letter by the predefined shift 
+                           cipheredWord += alpha[l - shift]
+                print(listDef[i])
+                listDef[i] = cipheredWord
+                print(listDef[i])
+            # check synonyms
+            for j in range(len(self.synonyms)) :
+                if listDef[i].lower == self.synonyms[j].lower():
+                    cipheredWord = ""
+                # for all letters in listDef[i]
+                for k in range(len(listDef[i])):
+                   # for all letters in the alphabet
+                   for l in range(len(alpha)):
+                       # if the current letter matches this letter of the alphabet   
+                        if (alpha[l].lower() == listDef[i][k].lower()):
+                           # shift the letter by the predefined shift 
+                           cipheredWord += alpha[l - shift]
+                print(listDef[i])
+                listDef[i] = cipheredWord
+                print(listDef[i])
+
+        # unsplit listDef yeah        
+        output = " ".join(listDef)
+        print(output)
+        return output
+        
+        
+    def getDefinition(self, word):
+        definition = dictionary.meaning('en', word)
+        definition = definition[1]
+        print(definition)
+        return definition
 
 
-def timer():
-    my_time = 60
-    for x in range(my_time, 0,-1):
-        seconds = x % 60
-        minutes = int(x / 60)  
-        print(f"{minutes:02}:{seconds:02}")
-        time.sleep(1)
+    def __init__(self,word):
+        self.word = word
+        self.synonyms = self.getSynonyms(word)
+        self.definition = self.getDefinition(word)
+        self.cipherDefinition = self.getCipherDefinition(self.definition)
+
+    def startGame(self):
+        guesses = 10
+        print("Welcome to Decryptionary!")
+        while (guess < 10 ):
+            ans = str(input("Enter your guess: "))
+            if (self.word.lower() == ans.lower()):
+                    flag = true
+                    break
+                    guess -= 1
+            else:
+                flag = false
+                print("Wrong! Try again")
+                print()
+                guess -= 1
+
+
     
-
-def points():
-    pass
-
-timer()
-
-
